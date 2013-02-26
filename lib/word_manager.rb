fb8aa5096ac6c2893849a3a208db8eebadc73876
Marcie Morton & George Michel <student@epicodus.com>
2013-02-26 15:06:16 -0800

    specs and functionality added to word manager class

--- lib/word_manager.rb
+++ lib/word_manager.rb
@@ -2,10 +2,23 @@ class WordManager
   attr_reader :all
   def initialize(words)
     @all = words
+    @current_word = ""
+    new_word
+    @guesses = []
   end
 
-  def random
-    @all.sample
+  def new_word
+    @current_word = @all.sample
+  end
+
+  def to_s
+    @current_word.split('').map do |letter|
+      @guesses.include?(letter) ? letter : '_'
+    end.join(' ')
+  end
+
+  def guess(letter)
+    @guesses << letter
   end
 
 end
--- spec/word_manager_spec.rb
+++ spec/word_manager_spec.rb
@@ -8,11 +8,39 @@ describe WordManager do
     end
   end
 
-  context '#random' do
-    it 'returns a random word from its store' do
+  context '#new_word' do
+    it 'returns a new word at random from its store' do
       word_manager = WordManager.new(['another','word'])
-      word_manager.all.should include(word_manager.random)
+      word_manager.all.should include(word_manager.new_word)
     end
+  end
+
+  context '#display' do
+    it 'displays a number of spaces corresponding to the letters in a selected word' do
+      Array.any_instance.stub(:sample).and_return('cat')
+      word_manager = WordManager.new([''])
+      word_manager.to_s.should eq '_ _ _'
+    end
+
+    it 'substitutes a letter for a space when a correct choice is made' do
+      Array.any_instance.stub(:sample).and_return('cat')
+      word_manager = WordManager.new([''])
+      word_manager.guess('c')
+      word_manager.to_s.should eq 'c _ _'
+    end
+
+    it 'returns word completed message to SOMEWHERE if all correct letters are chosen (no more spaces)' do
+      Array.any_instance.stub(:sample).and_return('cat')
+      word_manager = WordManager.new([''])
+      word_manager.winner?.should be false
+      word_manager.guess('c')
+      word_manager.guess('a')
+      word_manager.guess('t')
+      word_manager.winner?.should be true
+    end
+    it 'returns guess again message if letter has already been used/guessed'
+
+
   end
 
 end
